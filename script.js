a7feee2c03864f4ea70c657e53d340191a3afd88
kauanballan-glitch <kauan.ballan@escola.pr.gov.br>
2026-06-07 16:23:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
-function mostrarMensagem(){
+const noticias = [
+{
+titulo: "Paraná + Sustentável",
+texto: "Nova plataforma reúne dados ambientais e produtivos para apoiar agricultores."
+},
+{
+titulo: "Show Rural Coopavel 2026",
+texto: "Evento destacou inovação, sustentabilidade e agricultura familiar."
+},
+{
+titulo: "Bioinsumos ganham destaque",
+texto: "Encontro de Agroecologia reforçou a importância dos bioinsumos para uma agricultura mais sustentável."
+},
+{
+titulo: "Safra Paranaense em Crescimento",
+texto: "Boletins agrícolas apontam crescimento da produção com foco em eficiência e sustentabilidade."
+}
+];
 
-  const mensagem = document.getElementById("mensagem");
+function carregarNoticias(){
 
-  if(mensagem.style.display === "block"){
-    mensagem.style.display = "none";
-  } else {
-    mensagem.style.display = "block";
-  }
+const container = document.getElementById("news-container");
+
+container.innerHTML = "";
+
+noticias.forEach(noticia => {
+
+const div = document.createElement("div");
+
+div.classList.add("news");
+
+div.innerHTML = `
+<h3>${noticia.titulo}</h3>
+<p>${noticia.texto}</p>
+`;
+
+container.appendChild(div);
+
+});
 
 }
+
+window.onload = carregarNoticias;
